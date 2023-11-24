14a726b8f05d612b3e6283ddf591a295b468b59d
Fedor Burhonov <fedor.burgonov@mdfin.com>
2023-11-24 18:20:06 +0200

    Upload project

--- app.js
+++ app.js
@@ -35,16 +35,6 @@ const server = app.listen(443, (a, b, c, d) => {
   },
   server
 ); */
-
-app.get('/', (req, res) => res.type('html').send(html));
-
-/* const server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-); */
-
-// server.keepAliveTimeout = 120 * 1000;
-// server.headersTimeout = 120 * 1000;
-
 const html = `
 <!DOCTYPE html>
 <html>
@@ -95,3 +85,18 @@ const html = `
   </body>
 </html>
 `;
+
+app.get('/', (req, res) => {
+  // res.type('html').send(html);
+  res.send({
+    status: 200,
+    port,
+  });
+});
+
+/* const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+); */
+
+// server.keepAliveTimeout = 120 * 1000;
+// server.headersTimeout = 120 * 1000;
